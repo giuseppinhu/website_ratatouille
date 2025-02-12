2849940681aa87b266dea5ed6f02adb425b934aa
Giuseppe <giuseppe.ribeiro.silva@gmail.com>
2025-02-12 19:56:55 -0400

    add new section and fix size

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp'); 
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/**/*.scss')
-    .pipe(sass({style: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/'))
+        .pipe(sass({style: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/'))
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(images, styles);
+exports.images = images;
+
 exports.watch = function() {
     gulp.watch('./src/**/*.scss', styles);
 }
