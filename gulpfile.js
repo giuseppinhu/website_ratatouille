2afcbe6aafd95538da94619c5b172acbea67d50c
Giuseppe <giuseppe.ribeiro.silva@gmail.com>
2025-02-18 15:51:58 -0400

    fix media query and add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp'); 
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/**/*.scss')
         .pipe(sass({style: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/'))
+}
+
+function scripts() {
+    return gulp.src('./src/**/*.js')
+        .pipe(uglify())
         .pipe(gulp.dest('./dist/'))
 }
 
@@ -14,8 +21,8 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(images, styles);
-exports.images = images;
+exports.default = gulp.parallel(images, styles, scripts);
+exports.scripts = scripts;
 
 exports.watch = function() {
     gulp.watch('./src/**/*.scss', styles);
